bf00689b60722fc4ab02da9701efe28470cf9c6d
Snehasish <snehasishmohanty9439@gmail.com>
2024-09-02 15:05:27 +0530

    third-commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel.js";
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 import validator from 'validator'
 
 
@@ -14,7 +14,7 @@ const loginUser = async (req, res) => {
         if (!user) {
             return res.json({ success: false, message: "User doesn't exist" })
         }
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await bcryptjs.compare(password, user.password);
 
         if (!isMatch) {
             return res.json({ success: false, message: "Invalid credentials" })
@@ -57,8 +57,8 @@ const registerUser = async (req, res) => {
 
         //hashing user password
 
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt);
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt);
 
         const newUser = new userModel({
             name: name,
