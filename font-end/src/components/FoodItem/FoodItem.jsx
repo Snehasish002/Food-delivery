19c72f354150b8f3e7d3ef89b50eb9920003acce
Snehasish <snehasishmohanty9439@gmail.com>
2024-09-02 16:51:22 +0530

    fifth-commit

--- font-end/src/components/FoodItem/FoodItem.jsx
+++ font-end/src/components/FoodItem/FoodItem.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import "./FoodItem.css";
+import "./Fooditem.css";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
